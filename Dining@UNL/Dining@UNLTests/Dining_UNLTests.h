c3ecb0b44dcb5e177d3f512a292189f5b6c0697e
Jerrad Thramer <Jerrad.Thramer@gmail.com>
2011-11-14 14:46:46 -0600

    Initial Population & Commit.

---
+++ Dining@UNL/Dining@UNLTests/Dining_UNLTests.h
@@ -0,0 +1,13 @@
+//
+//  Dining_UNLTests.h
+//  Dining@UNLTests
+//
+//  Created by Jerrad Thramer on 11/14/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Dining_UNLTests : SenTestCase
+
+@end
---
+++ Dining@UNL/Dining@UNLTests/Dining_UNLTests.m
@@ -0,0 +1,32 @@
+//
+//  Dining_UNLTests.m
+//  Dining@UNLTests
+//
+//  Created by Jerrad Thramer on 11/14/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Dining_UNLTests.h"
+
+@implementation Dining_UNLTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Dining@UNLTests");
+}
+
+@end
